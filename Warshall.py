062260f7d231cf0852e6c8d76b9c42fbcbfbb1f6
LXvsqz <avasquez31@miners.utep.edu>
2020-03-08 23:38:14 -0600

    removed previous serial version and used pseudocode provided. Lab done, need report.

--- Warshall.py
+++ Warshall.py
@@ -1,44 +1,59 @@
-
 from mpi4py import MPI
 
-def genMatrix(size=10, value=5):
-    """
-    Generates a 2d square matrix of the specified size with the specified values
-    """
 
-    matrix = [[value for col in range(0,size)] for row in range(0,size)]
-
-    return matrix
+MPIMatrix = [[5,3,4,0,4], [9,4,5,2,2], [0,2,0,1,4],[1,4,9,1,7], [1,4,9,1,7] ] 
 
 
-testMatrix2 = [[5,3,4,0], [9,4,5,2], [0,2,0,1]]
-
-def warshall_Algorithm(matrix):
-
-    length = len(matrix)
-
-    for k in range(length):
-        for i in range(length):
-            for j in range(length):
-                matrix[i][j] = min(int(matrix[i][j]), int(matrix[i][k]) + int(matrix[k][j]))
-                
-    print(matrix)
+print("Input Matrix (5x5)")
+print(MPIMatrix)
+print("-----------------------------------")
 
 
 
+comm = MPI.COMM_WORLD
 
 
 
-#Number of rows per threads= number of rows / # of threads
-#Threads per row = # of threads/ number of rows
-#Starting row = number of rows per threads* process #
-#Ending row = number of rows per threads* (threads# + 1)
-#Owner of a row = (threads per row * row)
+length = len(MPIMatrix) #length of matrix (rows)
+
+threads_Per_Row = comm.Get_size() / length #helpful formulas from David 
+rows_Per_Thread = length // comm.Get_size()
+
+startRow = rows_Per_Thread * comm.Get_rank()
+endRow = rows_Per_Thread * (comm.Get_rank() + 1)
 
 
-testMatrix = genMatrix()
 
-warshall_Algorithm(testMatrix2)
+for k in range(length): #MPI Warshall
+
+    owner = int((comm.Get_size() / length) * k)
+    MPIMatrix[k] = comm.bcast(MPIMatrix[k], root=owner)
+
+    for x in range(startRow, endRow):
+        for y in range(length):
+            
+            MPIMatrix[x][y] = min(int(MPIMatrix[x][y]), (int(MPIMatrix[x][k]) + int(MPIMatrix[k][y])))
+
+
+
+#PUT MATRIX TOGETHER
+
+if comm.Get_rank() == 0:
+    for k in range(endRow, length):
+        owner = int((comm.Get_size() / length) * k) 
+        MPIMatrix[k] = comm.recv(source = owner, tag = k)
+        
+else:
+    for k in range(startRow, endRow):
+        owner = int((comm.Get_size() / length) * k) 
+        comm.send(MPIMatrix[k], dest = 0, tag = k)
+    
+  
+  
+  
+
+print("Warshall Algorithm")
+print(MPIMatrix)
 
 
 
